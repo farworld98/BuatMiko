0e22f22e0fdffebd61040149fca0ad6e088413b7
Tersakiti404-cyber <76648497+Tersakiti404-cyber@users.noreply.github.com>
2021-02-05 18:55:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log('Starting...')
 let { spawn } = require('child_process')
 let path = require('path')
 const CFonts  = require('cfonts')
-CFonts.say('Fajar And Debby', {
+CFonts.say('Fajar And Randi', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
